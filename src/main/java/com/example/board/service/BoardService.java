d42f1fbd42986b1f08ead2b8ca85a0ca7b4ef6ee
juni1015 <jiyoon6293@naver.com>
2023-06-07 17:25:24 +0900

    board_다중 파일 저장 기능 추가

--- src/main/java/com/example/board/service/BoardService.java
+++ src/main/java/com/example/board/service/BoardService.java
@@ -8,6 +8,7 @@ import com.example.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
@@ -23,7 +24,7 @@ public class BoardService {
     public Long save(BoardDTO boardDTO) throws IOException {
 //        BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
 //        boardRepository.save(boardEntity);
-        if (boardDTO.getBoardFile().isEmpty()) {
+        if (boardDTO.getBoardFile().get(0).isEmpty()) {
             // 파일 없음
             BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
             return boardRepository.save(boardEntity).getId();
@@ -33,18 +34,21 @@ public class BoardService {
             BoardEntity boardEntity = BoardEntity.toSaveEntityWiteFile(boardDTO);
             BoardEntity savedEntity = boardRepository.save(boardEntity);
             // 2. 파일이름 꺼내고, 저장용 이름 만들고 파일 로컬에 저장
-            String originalFileName = boardDTO.getBoardFile().getOriginalFilename();
-            String storedFileName = System.currentTimeMillis() + "_" + originalFileName;
-            String savePath = "D:\\springboot_img\\" + storedFileName;
-            boardDTO.getBoardFile().transferTo(new File(savePath));
-            // 3. BoardFileEntity로 변환 후 board_file_table에 저장
-            // 자식 데이터를 저장할 때 반드시 부모의 id가 아닌 부모의 Entity 객체가 전달돼야 함
-            BoardFileEntity boardFileEntity = BoardFileEntity.toSaveBoardFileEntity(savedEntity, originalFileName, storedFileName);
-            boardFileRepository.save(boardFileEntity);
+            for (MultipartFile boardFile : boardDTO.getBoardFile()) {
+                String originalFileName = boardFile.getOriginalFilename();
+                String storedFileName = System.currentTimeMillis() + "_" + originalFileName;
+                String savePath = "D:\\springboot_img\\" + storedFileName;
+                boardFile.transferTo(new File(savePath));
+                // 3. BoardFileEntity로 변환 후 board_file_table에 저장
+                // 자식 데이터를 저장할 때 반드시 부모의 id가 아닌 부모의 Entity 객체가 전달돼야 함
+                BoardFileEntity boardFileEntity = BoardFileEntity.toSaveBoardFileEntity(savedEntity, originalFileName, storedFileName);
+                boardFileRepository.save(boardFileEntity);
+            }
             return savedEntity.getId();
         }
     }
 
+    @Transactional
     public List<BoardDTO> findAll() {
         List<BoardDTO> boardDTOList = new ArrayList<>();
         List<BoardEntity> boardEntityList = boardRepository.findAll();
